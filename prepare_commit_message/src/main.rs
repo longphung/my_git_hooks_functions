1fab603ba75f0bf3c097a5a3a30f1e9bf700edcc
Phung Nguyen <longphung.nn@gmail.com>
2023-03-25 15:32:51 +1100

    ✨ feat: detect if in rebase and not run emoji attacher

--- prepare_commit_message/src/main.rs
+++ prepare_commit_message/src/main.rs
@@ -1,5 +1,6 @@
 mod append_to_git_message;
 
+use std::{fs, env};
 use append_to_git_message::append_to_git_message;
 use prepare_commit_message::emojis_attacher::EmojisAttacher;
 use prepare_commit_message::CommitMsgParams;
@@ -9,10 +10,15 @@ fn main() {
     let commit_message_params = CommitMsgParams::new();
 
     println!("{:#?}", commit_message_params);
+    let entries = fs::read_dir(".git").expect("No .git directory found");
+    // check if any of the entries have the string "rebase"
+    let is_rebase = entries
+        .map(|entry| entry.unwrap().file_name().into_string().unwrap())
+        .any(|entry| entry.contains("rebase"));
+    print!("{}", is_rebase);
 
-    // TODO: handle, do not run on rebase picks
-    if commit_message_params.commit_source == "template"
-        || commit_message_params.commit_source == "message"
+    if !is_rebase && (commit_message_params.commit_source == "template"
+        || commit_message_params.commit_source == "message")
     {
         messages.push(EmojisAttacher::init());
     }

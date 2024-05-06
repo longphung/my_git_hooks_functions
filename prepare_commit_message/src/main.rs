dfaaf012a79f1c60c853a242da90e2129afe2300
Phung Nguyen <longphung.nn@gmail.com>
2024-05-06 14:16:32 +1000

    ✨ feat: update for when not git dir is found

--- prepare_commit_message/src/main.rs
+++ prepare_commit_message/src/main.rs
@@ -1,6 +1,6 @@
 mod append_to_git_message;
 
-use std::{fs, env};
+use std::{fs};
 use append_to_git_message::append_to_git_message;
 use prepare_commit_message::emojis_attacher::EmojisAttacher;
 use prepare_commit_message::CommitMsgParams;
@@ -10,12 +10,18 @@ fn main() {
     let commit_message_params = CommitMsgParams::new();
 
     println!("{:#?}", commit_message_params);
-    let entries = fs::read_dir(".git").expect("No .git directory found");
-    // check if any of the entries have the string "rebase"
-    let is_rebase = entries
-        .map(|entry| entry.unwrap().file_name().into_string().unwrap())
-        .any(|entry| entry.contains("rebase"));
-    print!("{}", is_rebase);
+    // Skip if no .git directory is found
+    let entries = fs::read_dir(".git");
+    let mut is_rebase = false;
+
+    if entries.is_ok() {
+        // check if any of the entries have the string "rebase"
+        is_rebase = entries
+            .expect("Error reading .git directory")
+            .map(|entry| entry.unwrap().file_name().into_string().unwrap())
+            .any(|entry| entry.contains("rebase"));
+        print!("{}", is_rebase);
+    }
 
     if !is_rebase && (commit_message_params.commit_source == "template"
         || commit_message_params.commit_source == "message")
